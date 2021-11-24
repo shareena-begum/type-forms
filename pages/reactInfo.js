f58ff63b78ed1578dcfc1bfbb8959050603d4327
shareena <shareena.fsp@gmail.com>
2021-11-24 17:34:38 +0100

    reactinfo

---
+++ pages/reactInfo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "../public/src/ReactInfo/Navbar"
+import Main from "../public/src/ReactInfo/Main"
+
+function reactInfo(){
+    return (
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+        
+    )
+}
+
+export default reactInfo
