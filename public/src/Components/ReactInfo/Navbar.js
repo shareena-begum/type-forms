9cb44ee6dd2d54adb9b0d9eee4692d53eb2a0e86
shareena <shareena.fsp@gmail.com>
2021-11-25 17:19:12 +0100

    files shifted to component folder

--- public/src/Components/ReactInfo/Navbar.js
+++ public/src/Components/ReactInfo/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Image from "next/image"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="./images/download.png" className="nav--icon" />
+            <Image src="/logo.png" alt="react logo" width={50} height={50} priority />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
