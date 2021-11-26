114b503ddfe20c59aa0b9c6e7bb3d3e62705c7fc
shareena <shareena.fsp@gmail.com>
2021-11-26 20:39:32 +0100

    navbar and hero added

--- pages/airbnb.js
+++ pages/airbnb.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar1 from "../public/src/Components/AirBnb/Navbar1";
+import Hero from "../public/src/Components/AirBnb/Hero";
 
 function airbnb() {
     return(
-        <div>Airbnb</div>
+        <div>
+            <Navbar1 />
+            <Hero />
+        </div>
     )
 }
 
