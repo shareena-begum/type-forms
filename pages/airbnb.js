de6a8ecc217370e9fce2992202b8442df9ae7f51
shareena <shareena.fsp@gmail.com>
2021-12-01 16:48:36 +0100

    card component

--- pages/airbnb.js
+++ pages/airbnb.js
@@ -1,12 +1,32 @@
 import React from "react";
-import Navbar1 from "../public/src/Components/AirBnb/Navbar1";
-import Hero from "../public/src/Components/AirBnb/Hero";
+import Navbar1 from "../public/src/AirBnb/Navbar1";
+import Hero from "../public/src/AirBnb/Hero";
+import Card from "../public/src/AirBnb/Card";
+import data from "../public/src/data/data";
 
 function airbnb() {
+    const cards = data.map(item => {
+        return(
+            <Card 
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+                openSpots={item.openSpots}
+            
+            />
+        )
+    })
     return(
         <div>
             <Navbar1 />
-            <Hero />
+            {/* <Hero /> */}
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
