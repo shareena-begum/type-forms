a0b0ba96ffc6fa850580b48c5c834ca8e948cd25
shareena <shareena.fsp@gmail.com>
2021-12-01 16:07:45 +0100

    images transfered

--- public/src/Components/AirBnb/Hero.js
+++ public/src/Components/AirBnb/Hero.js
@@ -1,10 +1,10 @@
 import React from "react"
-import Image from "next/image"
+
 
 export default function Hero() {
     return (
         <section>
-            <Image src="/photo-grid.png" width={300} height={300}  className="hero--photo" />
+            <img src="/photo-grid.png" />
             <h1 className="hero--header">Online Experiences</h1>
             <p>Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
--- public/src/Components/AirBnb/Navbar1.js
+++ public/src/Components/AirBnb/Navbar1.js
@@ -1,10 +1,11 @@
 import React from "react"
-import Image from "next/image"
+// import Image from "next/image"
 
 function Navbar1() {
     return (
         <nav>
-            <Image src="/airbnb-logo.png" alt="Airbnb logo" width={128} height={77}  className="nav--logo"/>
+            <img src="/airbnb-logo.png" /> 
+            
         </nav>
     )
 }
